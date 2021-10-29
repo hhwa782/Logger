4be4482fef9a4dc9718feb1052b87620e711e7ed
hhwa782 <93207559+hhwa782@users.noreply.github.com>
2021-10-30 02:31:49 +0900

    Update Logger.cs

--- Logger.cs
+++ Logger.cs
@@ -3,35 +3,53 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
-namespace MyClient2
+namespace ClientProgram
 {
     class Logger
     {
-        StreamWriter sw;
         // Default
-        static int OpenLogFile(string opf)
+        public static int OpenLogFile()
+        {
+            // Log 폴더 유뮤 확인 후 생성
+            string logDir = Path.Combine(Environment.CurrentDirectory, "Log", DateTime.Now.ToString("yyyy-MM"));
+            if (Directory.Exists(logDir) == false) Directory.CreateDirectory(logDir);
+
+            // 현재 기준의 [일(d)-시(H)] 파일 유무 확인 후 생성
+            if (File.Exists(filePath()) == false) File.Create(filePath());
+
+            return 0;
+        }
+
+        public static int CloseLogFile()
+        {
+            using (var sw = new StreamWriter(filePath(), true, Encoding.UTF8))
+            {
+                sw.Close();
+            }
+
+            return 0;
+        }
+
+        public static int Write(string sender, string wr)
+        {
+            string log = DateTime.Now.ToString("[yyyy-MM-dd HH:mm:ss]") + "<" + sender + "가 보냄> " + wr;
+
+            using (var sw = new StreamWriter(filePath(), true, Encoding.UTF8))
+            {
+                Console.WriteLine(log);
+                sw.WriteLine(log);
+            }
+
+            return 0;
+        }
+
+        // Plus
+        public static string filePath()
         {
             string logDir = Path.Combine(Environment.CurrentDirectory, "Log", DateTime.Now.ToString("yyyy-MM"));
             string logFile = Path.Combine(logDir, DateTime.Now.ToString("dd-HH") + ".txt");
 
-            if (Directory.Exists(logDir) == false) Directory.CreateDirectory(logDir);
-            if (File.Exists(logFile) == false) File.Create(logFile);
-
-            return 0;
-        }
-
-        static int CloseLogFile()
-        {
-            //sw.Close();
-
-            return 0;
-        }
-
-        static int Write(string wr)
-        {
-            //sw.WriteLine(wr);
-            
-            return 0;
+            return logFile;
         }
     }
 }
